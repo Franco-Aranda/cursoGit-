6a6d87aec913d139313344691e416b1f2e96d218
unknown <francoaranda.dv@gmail.com>
2021-01-29 14:04:30 -0300

    agregado de fecha de nacimiento

--- practica2.py
+++ practica2.py
@@ -1,6 +1,6 @@
 nombre="Franco Aranda"
 dni= 38100714
-
+fechaNacimiento= 2-5-1994
 
 print(f"Hola mi nombre es {nombre} y mi DNI es {dni}")
-
+print("Mi fecha de nacimiento es: ", + fechaNacimiento)
